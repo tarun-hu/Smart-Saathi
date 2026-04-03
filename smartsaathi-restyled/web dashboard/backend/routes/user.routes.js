d8a9ea51277dc74d70672a661aa548fa093c8683
KrishJaidka <106903875+KrishJaidka@users.noreply.github.com>
2026-04-03 10:05:05 +0530

    removed secondary dashboard

--- smartsaathi-restyled/web dashboard/backend/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createUser } = require("../controllers/user.controller");
-
-const router = express.Router();
-
-router.post("/", createUser);
-// router.get("/", getUsers);
-
-module.exports = router;
